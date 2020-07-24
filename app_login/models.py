86925810b5e24b0d4bf03c912b2b47982b872f4a
a-pompom <aoi.matsuda.3x@gmail.com>
2020-07-24 21:21:43 +0900

    ログインAPIの土台部分作成。
    ログインAPIの土台として、認証用ユーザを作成し、SQLiteのDBから参照できることを確認した。
    今回は実装をシンプルにしたかったので、認証用ユーザは、パスワードを持たないものとした。
    
    #3

---
+++ app_login/models.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from django.db import models
+from django.db.models import CharField
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.hashers import make_password
+
+class User(AbstractBaseUser):
+    """ 認証用ユーザ """
+
+    class Meta:
+        db_table = 'm_user'
+
+    USERNAME_FIELD = 'username'
+
+    # ユーザ名 ユニーク
+    username: CharField[str, str] = models.CharField(
+        name='username',
+        max_length=255,
+        unique=True,
+    )
+
+    # パスワード 今回のアプリでは使用せず、全ユーザ固定値
+    password: CharField[str, str] = models.CharField(
+        name='password',
+        max_length=255,
+        default=make_password('root')
+    )
+
+    # ユーザアイコンのパス
+    icon_path: CharField[str, str] = models.CharField(
+        name='icon_path',
+        max_length=1024,
+        null=True
+    )
+
+    def __str__(self):
+
+        return f'username: {self.username}'
--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+API_VERSION = 1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(f'api/v{API_VERSION}/login', include('app_login.urls')),
 ]
