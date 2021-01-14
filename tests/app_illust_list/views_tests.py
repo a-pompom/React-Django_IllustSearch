cf9d35c7528e400a3bfd30ca794bdecc289cebff
a-pompom <aoi.matsuda.3x@gmail.com>
2021-01-14 19:42:15 +0900

    Write Test Code for Category PUT/DELETE api
    カテゴリ更新・削除APIのテストコード作成

--- tests/app_illust_list/views_tests.py
+++ tests/app_illust_list/views_tests.py
@@ -1,9 +1,26 @@
 import pytest
+from pytest_mock import MockerFixture
 from rest_framework.status import HTTP_200_OK, HTTP_422_UNPROCESSABLE_ENTITY
 
-from .views_data import data_category_view
+from app_illust_list.models.category import Category
+from .views_data import data_category_view, MOCK_CATEGORY_UUID_LIST
 from tests.view_utils import test_common_view_function
 from tests.conftest import ParamViewRequestType
+
+# Viewテスト用パラメータ名
+VIEW_REQUEST_PARAMS = 'view_request_params'
+UUID_MOCK_MODULE_NAME = 'uuid.uuid4'
+
+class Test__for_mock_target:
+
+    def test__can_mock_category_id_default(self, mocker: MockerFixture):
+        # GIVEN
+        field = Category._meta.get_field('category_id')
+        mock_uuid = lambda: MOCK_CATEGORY_UUID_LIST[0]
+        # WHEN
+        mocker.patch.object(field, 'default', mock_uuid)
+        # THEN
+        assert Category().category_id == MOCK_CATEGORY_UUID_LIST[0]
 
 
 @pytest.mark.django_db(transaction=False)
@@ -12,19 +29,21 @@ class TestCategoryView:
     def test__common(self):
         test_common_view_function.run_tests({'login_path': data_category_view.get_path()})
 
+
     class Test__get:
 
         @pytest.mark.parametrize(
-            'view_request_params',
+            VIEW_REQUEST_PARAMS,
             [
                 pytest.param(data_category_view.get_success_get(), id='simple')
             ],
-            indirect=['view_request_params']
+            indirect=[VIEW_REQUEST_PARAMS]
         )
         def test__get_categories(self, view_request_params: ParamViewRequestType):
 
             # GIVEN
-            client, path, username, model_items_list, expected = view_request_params
+            params = view_request_params
+            client, path, username, expected = (params.client, params.path, params.login_username, params.expected_response)
             # WHEN
             client.login(username=username)
             actual = client.get(path)
@@ -32,60 +51,178 @@ class TestCategoryView:
             assert actual.status_code == HTTP_200_OK
             assert actual.data == expected
 
+
     class Test__post:
 
         @pytest.mark.parametrize(
-            'view_request_params',
+            VIEW_REQUEST_PARAMS,
             [
                 pytest.param(data_category_view.get_single_category(), id='simple')
             ],
-            indirect=['view_request_params']
+            indirect=[VIEW_REQUEST_PARAMS]
         )
-        def test__create_category(self, view_request_params: ParamViewRequestType):
+        def test__create_category(self, mocker: MockerFixture, view_request_params: ParamViewRequestType):
             # GIVEN
-            client, path, username, _, expected = view_request_params
+            params = view_request_params
+            client, path, username, post_body, expected = (params.client, params.path, params.login_username, params.post_body, params.expected_response)
+            # GIVEN-MOCK
+            field = Category._meta.get_field('category_id')
+            mock_uuid = lambda: MOCK_CATEGORY_UUID_LIST[0]
+            mocker.patch.object(field, 'default', mock_uuid)
             # WHEN
             client.login(username=username)
-            actual = client.post(path, {'category_name': '実写'})
+            actual = client.post(path, post_body)
             # THEN
             assert actual.status_code == HTTP_200_OK
             assert actual.data == expected
 
         @pytest.mark.parametrize(
-            'view_request_params',
+            VIEW_REQUEST_PARAMS,
             [
-                pytest.param(data_category_view.get_single_category(), id='simple')
+                pytest.param(data_category_view.get_empty_category_post(), id='simple')
             ],
-            indirect=['view_request_params']
+            indirect=[VIEW_REQUEST_PARAMS]
         )
         def test__empty(self, view_request_params: ParamViewRequestType):
             # GIVEN
-            client, path, username, _, expected = view_request_params
+            params = view_request_params
+            client, path, username, post_body, expected = (params.client, params.path, params.login_username, params.post_body, params.expected_response)
             # WHEN
             client.login(username=username)
-            actual = client.post(path, {'category_name': ''})
+            actual = client.post(path, post_body)
             # THEN
             assert actual.status_code == HTTP_422_UNPROCESSABLE_ENTITY
-            assert actual.data['errors'][0]['message'] == 'カテゴリ名を入力してください。'
+            assert actual.data == expected
 
         @pytest.mark.parametrize(
-            'view_request_params',
+            VIEW_REQUEST_PARAMS,
             [
-                pytest.param(data_category_view.get_single_category(), id='simple')
+                pytest.param(data_category_view.get_duplicate_category_post(), id='simple')
             ],
-            indirect=['view_request_params']
+            indirect=[VIEW_REQUEST_PARAMS]
         )
         def test__duplicate(self, view_request_params: ParamViewRequestType):
             # GIVEN
-            client, path, username, _, expected = view_request_params
+            params = view_request_params
+            client, path, username, post_body, expected = (params.client, params.path, params.login_username, params.post_body, params.expected_response)
             # WHEN
             client.login(username=username)
             # 二重登録となるよう、二回Post
-            client.post(path, {'category_name': '実写'})
-            actual = client.post(path, {'category_name': '実写'})
+            client.post(path, post_body)
+            actual = client.post(path, post_body)
             # THEN
             assert actual.status_code == HTTP_422_UNPROCESSABLE_ENTITY
-            assert actual.data['errors'][0]['message'] == 'カテゴリ名は既に使用されています。'
+            assert actual.data == expected
+
+
+    class Test__put:
+        
+        @pytest.mark.parametrize(
+            VIEW_REQUEST_PARAMS,
+            [
+                pytest.param(data_category_view.get_single_category_put(), id='simple')
+            ],
+            indirect=[VIEW_REQUEST_PARAMS]
+        )
+        def test__update_category(self, mocker: MockerFixture, view_request_params: ParamViewRequestType):
+            # GIVEN
+            params = view_request_params
+            client, path, username, post_body, put_body, expected = (params.client, params.path, params.login_username, params.post_body, params.put_body, params.expected_response)
+            # GIVEN-MOCK
+            field = Category._meta.get_field('category_id')
+            mock_uuid = lambda: MOCK_CATEGORY_UUID_LIST[0]
+            mocker.patch.object(field, 'default', mock_uuid)
+            # WHEN
+            client.login(username=username)
+            client.post(path, post_body)
+            actual = client.put(path, put_body)
+            # THEN
+            assert actual.status_code == HTTP_200_OK
+            assert actual.data == expected
+        
+        @pytest.mark.parametrize(
+            VIEW_REQUEST_PARAMS,
+            [
+                pytest.param(data_category_view.get_empty_category_name_put(), id='empty')
+            ],
+            indirect=[VIEW_REQUEST_PARAMS]
+        )
+        def test__empty_category_name(self, view_request_params: ParamViewRequestType):
+            # GIVEN
+            params = view_request_params
+            client, path, username, post_body, put_body, expected = (params.client, params.path, params.login_username, params.post_body, params.put_body, params.expected_response)
+            # WHEN
+            client.login(username=username)
+            client.post(path, post_body)
+            actual = client.put(path, put_body)
+            # THEN
+            assert actual.status_code == HTTP_422_UNPROCESSABLE_ENTITY
+            assert actual.data == expected
+
+        @pytest.mark.parametrize(
+            VIEW_REQUEST_PARAMS,
+            [
+                pytest.param(data_category_view.get_empty_category_id_put(), id='empty_category_id')
+            ],
+            indirect=[VIEW_REQUEST_PARAMS]
+        )
+        def test__empty_category_id(self, view_request_params: ParamViewRequestType):
+            # GIVEN
+            params = view_request_params
+            client, path, username, post_body, put_body, expected = (params.client, params.path, params.login_username, params.post_body, params.put_body, params.expected_response)
+            # WHEN
+            client.login(username=username)
+            client.post(path, post_body)
+            actual = client.put(path, put_body)
+            # THEN
+            assert actual.status_code == HTTP_422_UNPROCESSABLE_ENTITY
+            assert actual.data == expected
+
+    
+    class Test__delete:
+
+        @pytest.mark.parametrize(
+            VIEW_REQUEST_PARAMS,
+            [
+                pytest.param(data_category_view.get_single_category_delete(), id='simple')
+            ],
+            indirect=[VIEW_REQUEST_PARAMS]
+        )
+        def test__success_delete(self, mocker: MockerFixture, view_request_params: ParamViewRequestType):
+            # GIVEN
+            params = view_request_params
+            client, path, username, post_body, delete_body, expected = (params.client, params.path, params.login_username, params.post_body, params.delete_body, params.expected_response)
+            # GIVEN-MOCK
+            field = Category._meta.get_field('category_id')
+            mock_uuid = lambda: MOCK_CATEGORY_UUID_LIST[0]
+            mocker.patch.object(field, 'default', mock_uuid)
+            # WHEN
+            client.login(username=username)
+            client.post(path, post_body)
+            actual = client.delete(path, delete_body)
+            # THEN
+            assert actual.status_code == HTTP_200_OK
+            assert actual.data == expected
+
+
+        @pytest.mark.parametrize(
+            VIEW_REQUEST_PARAMS,
+            [
+                pytest.param(data_category_view.get_empty_category_id_delete(), id='empty_category_id')
+            ],
+            indirect=[VIEW_REQUEST_PARAMS]
+        )
+        def test__empty_category_id(self, view_request_params: ParamViewRequestType):
+            # GIVEN
+            params = view_request_params
+            client, path, username, post_body, delete_body, expected = (params.client, params.path, params.login_username, params.post_body, params.delete_body,  params.expected_response)
+            # WHEN
+            client.login(username=username)
+            client.post(path, post_body)
+            actual = client.delete(path, delete_body)
+            # THEN
+            assert actual.status_code == HTTP_422_UNPROCESSABLE_ENTITY
+            assert actual.data == expected
 
 
 # @pytest.mark.django_db(transaction=False)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Optional, Any
 import django
 # importを解決する段階で設定ファイルが読み込み済である必要があるため、
 # conftestのグローバル、すなわちテスト全体で最初に実行される部分に
@@ -8,23 +8,34 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
 django.setup()
 
 import pytest
+import dataclasses
 from _pytest.fixtures import SubRequest
 from django.db import models
 from rest_framework.test import APIClient
 
 from common.custom_type import TypeAPIResponse
 
-ParamViewRequestType = Tuple[APIClient, str, str, List[List[models.Model]], TypeAPIResponse]
+@dataclasses.dataclass
+class ParamViewRequestType:
+    """ View機能のテスト用パラメータ """
+    client: APIClient
+    path: str
+    login_username: str
+    model_items_list: List[List[models.Model]]
+    expected_response: TypeAPIResponse
+    get_query: str = ''
+    post_body: Optional[Dict[str, Any]] = None
+    put_body: Optional[Dict[str, Any]] = None
+    delete_body: Optional[Dict[str, Any]] = None
 
 
 @pytest.fixture
 def view_request_params(request: SubRequest) -> ParamViewRequestType:
 
     params: ParamViewRequestType = request.param
-    client, path, username, model_items_list, expected = params
 
     # DB事前登録
-    for model_items in model_items_list:
+    for model_items in params.model_items_list:
         [model.save() for model in model_items]
 
     return params
